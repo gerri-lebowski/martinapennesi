24612040cf96b6c36b75e39390c0ada52bff1b0c
gerri-lebowski <86618469+gerri-lebowski@users.noreply.github.com>
2025-12-13 00:47:10 +0100

    Update domain from martinapennesi.com to martinapennesi.it

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://martinapennesi.com',
+	site: 'https://martinapennesi.it',
 	integrations: [
 		sitemap({
 			changefreq: 'monthly',
